87922d25aff2a875a9e92ebf0432ad6a9697e5dc
Jacob Woliver <jacob@jmw.sh>
2023-12-22 18:46:10 -0500

    Release v0.2.0

--- Formula/hui.rb
+++ Formula/hui.rb
@@ -3,9 +3,9 @@
 class Hui < Formula
     desc "Visual alternative to history written in Rust"
     homepage "https://github.com/jmwoliver/hui"
-    url "https://github.com/jmwoliver/hui/releases/download/v0.1.1/hui-mac-aarch64-0.1.1.tar.gz"
-    sha256 "ea69b76f491b74ac98e85e7ac7e9f3155b56655972ef74eb657aa36ac4a46ba9"
-    version "0.1.1"
+    url "https://github.com/jmwoliver/hui/releases/download/v0.2.0/hui-mac-aarch64-0.2.0.tar.gz"
+    sha256 "b43130f648204899c5280dac58448a64ff0a8632261e97bf7e6b6b8941b783bc"
+    version "0.2.0"
 
     def install
       bin.install "hui"
